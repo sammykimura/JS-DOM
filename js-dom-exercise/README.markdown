c708ff7c2c6bbddc1e17fb9216d54158571dfe0f
sammykimura <91994204+sammykimura@users.noreply.github.com>
2021-11-04 16:40:09 -0400

    Add files via upload

---
+++ js-dom-exercise/README.markdown
@@ -0,0 +1,5 @@
+# JS DOM Exercise
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sammykimura/pen/OJjQpKL](https://codepen.io/sammykimura/pen/OJjQpKL).
+
+
---
+++ js-dom-exercise/src/script.js
@@ -0,0 +1,30 @@
+
+
+function countDiv1 () {
+  let count = document.getElementById("topSection").childElementCount;
+  let element=document.getElementById('div2');
+  element+=(`TopSection has ` + count + ` p elements`);
+  console.log(`TopSection has ` + count + ` p elements`);
+};
+
+myButton1.addEventListener("click", countDiv1);
+///////////////////////
+
+function countDiv2 () {
+  let count = document.getElementById("bottomSection").childElementCount;
+  let element=document.getElementById('div3');
+  element+=(`bottomSection has ` + count + ` p elements`);
+  console.log(`bottomSection has ` + count + ` p elements`);
+};
+
+myButton2.addEventListener("click", countDiv2);
+/////////////////////////
+
+function countElements() {
+  let count =element.getElementByTagName('p');
+  let element=document.getElementById('div1');
+  element+=(`There are ` + count + ` p elements`);
+  console.log(`There are ` + count + ` p elements`);
+};
+
+myButton3.addEventListener("click", countElements);
